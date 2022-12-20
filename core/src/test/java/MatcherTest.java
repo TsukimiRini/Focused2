c0680f012ef0f0d5b2da90ed2e986e21ec459c04
tannpopo <yuailun@pku.edu.cn>
2022-12-20 20:17:05 +0800

    test: add a test of uri variable matcher

--- core/src/test/java/MatcherTest.java
+++ core/src/test/java/MatcherTest.java
@@ -1,7 +1,9 @@
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static utils.MatcherUtils.*;
 
 public class MatcherTest {
@@ -17,5 +19,15 @@ public class MatcherTest {
     String seg = "This is a test::key1#func[val1=\\[abc\\],val2=123,val3=\\:def\\:]";
     Map<String, String> res = splitSegment(seg);
     System.out.println(res);
+  }
+
+  @Test
+  public void testURIAttr(){
+    String uriAttr = "def[widgetID]";
+    Pair<Boolean, String> res = parseURIRefAttrInConfig(uriAttr);
+    assertEquals(res.getRight(), "def");
+    uriAttr = "ref.file";
+    res = parseURIRefAttrInConfig(uriAttr);
+    assertEquals(res.getRight(), "ref");
   }
 }
