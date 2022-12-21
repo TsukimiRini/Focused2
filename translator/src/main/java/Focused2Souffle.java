f0639f9c1c05e88b65c67ea8f2f71d71c6502f86
tannpopo <yuailun@pku.edu.cn>
2022-12-22 00:51:56 +0800

    feat: support translate links in config file preliminary

--- translator/src/main/java/Focused2Souffle.java
+++ translator/src/main/java/Focused2Souffle.java
@@ -1,15 +1,14 @@
 import model.*;
+import model.config.ConfigLink;
 import model.config.ConfigLinkBlock;
-import model.souffle.ADT;
-import model.souffle.PredicateDecl;
-import model.souffle.Record;
+import model.enums.PredicateType;
+import model.souffle.*;
 import org.apache.commons.lang3.tuple.Pair;
 import utils.FileUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Focused2Souffle {
@@ -39,6 +38,14 @@ public class Focused2Souffle {
     File opt = FileUtil.createOrClearFile(souffle_output);
     FileUtil.appendTo(opt, types);
     FileUtil.appendTo(opt, predicates);
+
+    List<ConfigLinkBlock> blocks = pair.getRight();
+    Map<String, URIPattern> patternMap = new HashMap<>();
+    patterns.forEach(pattern -> patternMap.put(pattern.label, pattern));
+    List<Component> components = generateComponents(blocks, patternMap);
+    for (Component component : components) {
+      FileUtil.appendTo(opt, component.toString() + "\n");
+    }
   }
 
   public static Record generateURIType() {
@@ -83,5 +90,46 @@ public class Focused2Souffle {
       decls.add(new PredicateDecl(pattern.label, "uri", "URI"));
     }
     return decls;
+  }
+
+  public static List<Component> generateComponents(
+      List<ConfigLinkBlock> blocks, Map<String, URIPattern> patternMap) {
+    List<Component> components = new ArrayList<>();
+    for (ConfigLinkBlock block : blocks) {
+      components.add(getComponent(block, patternMap));
+    }
+    return components;
+  }
+
+  private static Component getComponent(ConfigLinkBlock block, Map<String, URIPattern> patternMap) {
+    Component cur =
+        new Component(
+            block.blockName,
+            block.baseBlocks.stream().map(base -> base.blockName).collect(Collectors.toList()));
+
+    for (String predicateName : block.predicateDecls.keySet()) {
+      PredicateDecl decl = new PredicateDecl(predicateName);
+      List<Set<String>> params = block.paramTypes.get(predicateName);
+      for (int i = 0; i < params.size(); i++) {
+        decl.addParam("param" + i, params.get(i).isEmpty() ? "symbol" : "URI");
+      }
+      cur.innerStmts.add(decl.toString());
+    }
+
+    for (String predicateName : block.predicateDecls.keySet()) {
+      List<ConfigLink> links = block.predicateDecls.get(predicateName);
+      for (ConfigLink link : links) {
+        cur.innerStmts.add(link.toSouffleCode(patternMap));
+      }
+    }
+
+    String instanceName = ((CompInitializer) cur.postfixStmts.get(0)).instanceName;
+    block.predicateTypes.forEach(
+        (predicateName, type) -> {
+          if (type == PredicateType.PUBLIC) {
+            cur.postfixStmts.add(new IOStmt(true, instanceName+"."+predicateName));
+          }
+        });
+    return cur;
   }
 }
