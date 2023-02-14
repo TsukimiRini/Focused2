401408fa2e48507fd515c06e2b9e5c0d3af883fc
tannpopo <yuailun@pku.edu.cn>
2023-02-14 21:26:05 +0800

    feat: extend file utils

--- core/src/main/java/utils/FileUtil.java
+++ core/src/main/java/utils/FileUtil.java
@@ -1,9 +1,19 @@
 package utils;
 
+import model.Language;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileUtil {
   public static File createOrClearFile(String path) throws IOException {
@@ -22,5 +32,160 @@ public class FileUtil {
       buff.write(part);
     }
     buff.close();
+  }
+
+  private static String rootPath;
+
+  public static void setRootPath(String rootPath) {
+    FileUtil.rootPath = rootPath;
+  }
+
+  public static String getRootPath() {
+    return rootPath;
+  }
+
+  /**
+   * Get path relative to the root path
+   *
+   * @param absolutePath
+   * @return
+   */
+  public static String getRelativePath(String absolutePath) {
+    return FilenameUtils.separatorsToUnix(FileUtil.getRelativePath(rootPath, absolutePath));
+  }
+
+  /**
+   * Get path relative to the root path
+   *
+   * @param absolutePath
+   * @param rootPath
+   * @return
+   */
+  public static String getRelativePath(String rootPath, String absolutePath) {
+    return Paths.get(rootPath).relativize(Paths.get(absolutePath)).toString();
+  }
+
+  /**
+   * Write the given content in the file of the given file path.
+   *
+   * @param content
+   * @param filePath
+   * @return boolean indicating the success of the write operation.
+   */
+  public static boolean writeStringToFile(String content, String filePath) {
+    try {
+      FileUtils.writeStringToFile(new File(filePath), content, "UTF-8");
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+  /**
+   * Read the content of a file into string Actually wraps commons io API
+   *
+   * @return
+   */
+  public static String readFileToString(String filePath) {
+    String content = "";
+    try {
+      content = FileUtils.readFileToString(new File(filePath), "UTF-8");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return content;
+  }
+
+  public static Map<String, List<String>> categorizeFilesByExtension(List<String> filePaths) {
+    Map<String, List<String>> result = new LinkedHashMap<>();
+    for (String path : filePaths) {
+      String extension = FilenameUtils.getExtension(path);
+      if (!result.containsKey(extension)) {
+        result.put(extension, new ArrayList<>());
+      }
+      result.get(extension).add(path);
+    }
+    return result;
+  }
+
+  public static Map<String, List<String>> categorizeFilesByExtensionInLanguages(
+          List<String> filePaths, Set<Language> languages) {
+    Set<String> extensions =
+            languages.stream()
+                    .map(language -> language.extension.replace(".", ""))
+                    .collect(Collectors.toSet());
+
+    Map<String, List<String>> result = new LinkedHashMap<>();
+    for (String path : filePaths) {
+      String extension = FilenameUtils.getExtension(path);
+      if (!extensions.contains((extension))) continue;
+      if (!result.containsKey(extension)) {
+        result.put(extension, new ArrayList<>());
+      }
+      result.get(extension).add(path);
+    }
+    return result;
+  }
+
+  /**
+   * List all files with specific extension under a folder/directory
+   *
+   * @param dir
+   * @return absolute paths
+   */
+  public static List<String> listFilePaths(String dir, String extension) {
+    List<String> result = new ArrayList<>();
+    try (Stream<Path> walk = Files.walk(Paths.get(dir))) {
+      if (extension.isEmpty() || extension.isBlank()) {
+        result = walk.filter(Files::isRegularFile).map(Path::toString).collect(Collectors.toList());
+      } else {
+        result =
+                walk.filter(Files::isRegularFile)
+                        .map(Path::toString)
+                        .filter(path -> path.endsWith(extension))
+                        //              .map(s -> s.substring(dir.length()))
+                        .collect(Collectors.toList());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return result;
+  }
+
+  /**
+   * List { extension : [filePath] } under a folder/directory
+   *
+   * @param dir
+   * @param languages
+   * @return
+   */
+  public static Map<String, List<String>> listFilePathsInLanguages(
+          String dir, Set<Language> languages) {
+    if (languages.isEmpty()) {
+      return new HashMap<>();
+    }
+    Map<String, List<String>> result = new LinkedHashMap<>();
+
+    Set<String> extensions =
+            languages.stream()
+                    .map(language -> language.extension.replace(".", ""))
+                    .collect(Collectors.toSet());
+    try (Stream<Path> walk = Files.walk(Paths.get(dir))) {
+      walk.filter(Files::isRegularFile)
+              .map(Path::toString)
+              .forEach(
+                      path -> {
+                        String ext = FilenameUtils.getExtension(path);
+                        if (extensions.contains(ext)) {
+                          if (!result.containsKey(ext)) {
+                            result.put(ext, new ArrayList<>());
+                          }
+                          result.get(ext).add(path);
+                        }
+                      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return result;
   }
 }
